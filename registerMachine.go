3e9549af7525b66077838ec01015672c0a8d5371
Dan Walsh <dwalsh@redhat.com>
2015-10-29 16:38:06 -0400

    Make work with latest hooks patch

--- registerMachine.go
+++ registerMachine.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"log"
+	"log/syslog"
 	"os"
 
 	"github.com/godbus/dbus"
@@ -13,31 +14,11 @@ import (
 
 var conn *dbus.Conn
 
-func main() {
-	var jmap map[string]interface{}
-	json.NewDecoder(os.Stdin).Decode(&jmap)
-	pid := jmap["pid"].(float64)
-	id := jmap["id"].(string)
-	root_directory := jmap["root"].(string)
-	name := id
-
-	// ensure id is a hex string at least 32 chars
-	passId, err := Validate(id)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	if os.Args[0] == "prestart" {
-		if err = RegisterMachine(name, passId, int(pid), root_directory); err != nil {
-			log.Fatalf("Register machine failed: %v", err)
-		}
-	}
-
-	if os.Args[0] == "poststop" {
-		if err := TerminateMachine(name); err != nil {
-			log.Fatalf("TerminateMachine failed: %v", err)
-		}
-	}
+type State struct {
+	Version string `json:"version"`
+	ID      string `json:"id"`
+	Pid     int    `json:"pid"`
+	Root    string `json:"root"`
 }
 
 func Validate(id string) (string, error) {
@@ -69,6 +50,7 @@ func RegisterMachine(name string, id string, pid int, root_directory string) err
 	if service == "" {
 		service = "runc"
 	}
+	log.Print("RegisterMachine: objCall")
 	return obj.Call("org.freedesktop.machine1.Manager.RegisterMachine", 0, name, av, service, "container", uint32(pid), root_directory).Err
 	return nil
 }
@@ -85,4 +67,42 @@ func TerminateMachine(name string) error {
 	obj := conn.Object("org.freedesktop.machine1", "/org/freedesktop/machine1")
 	return obj.Call("org.freedesktop.machine1.Manager.TerminateMachine", 0, name).Err
 	return nil
+}
+
+func main() {
+	var state State
+	logwriter, err := syslog.New(syslog.LOG_NOTICE, "ociRegisterMachine")
+	if err == nil {
+		log.SetOutput(logwriter)
+	}
+	command := os.Args[1]
+	log.Print("oci register machine: ", command)
+	if err := json.NewDecoder(os.Stdin).Decode(&state); err != nil {
+		log.Fatalf("RegisterMachine Failed %v", err.Error())
+	}
+
+	log.Printf("Register machine: %s %d %s %s", command, state.ID, state.Pid, state.Root)
+	// ensure id is a hex string at least 32 chars
+	passId, err := Validate(state.ID)
+	if err != nil {
+		log.Fatalf("RegisterMachine Failed %v", err.Error())
+	}
+
+	switch command {
+	case "prestart":
+		{
+			if err = RegisterMachine(state.ID, passId, int(state.Pid), state.Root); err != nil {
+				log.Fatalf("Register machine failed: %v", err)
+			}
+			return
+		}
+	case "poststop":
+		{
+			if err := TerminateMachine(state.ID); err != nil {
+				log.Fatalf("TerminateMachine failed: %v", err)
+			}
+			return
+		}
+		log.Fatalf("Invalid command %q must be prestart|poststop", command)
+	}
 }
